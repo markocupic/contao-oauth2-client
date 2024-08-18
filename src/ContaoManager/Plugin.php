e1896606bea8427b40089b68b1d4d3d8cf59eb7c
Marko Cupic <m.cupic@gmx.ch>
2024-08-18 12:17:31 +0200

    Fix routing.

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -37,17 +37,12 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface, Extension
         ];
     }
 
-    /**
-     * @throws \Exception
-     *
-     * @return RouteCollection|null
-     */
     public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
     {
         return $resolver
-            ->resolve(__DIR__.'/../../config/routes.yaml')
-            ->load(__DIR__.'/../../config/routes.yaml')
-    ;
+            ->resolve(__DIR__.'/../Controller')
+            ->load(__DIR__.'/../Controller')
+            ;
     }
 
     public function getExtensionConfig($extensionName, array $extensionConfigs, ContainerBuilder $container): array
